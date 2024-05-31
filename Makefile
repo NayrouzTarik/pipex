4b9852ee59d266711b93f6de948ded4563c907d3
Nayrouz Tarik <ntarik@e1r4p4.1337.ma>
2024-05-31 15:36:40 +0100

    done

--- Makefile
+++ Makefile
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ntarik <ntarik@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/05/27 15:37:25 by ntarik            #+#    #+#              #
+#    Updated: 2024/05/27 18:32:50 by ntarik           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 NAME  = pipex
 
 INC = inc/pipex.h
@@ -6,9 +18,10 @@ CC = cc
 
 CFLAGS = -Wall -Wextra -Werror
 
-SRC = parcing.c \
-		ft_split.c \
+SRC = ft_split.c \
 		utils.c \
+		parse.c \
+		init_pipex.c \
 
 OBJ = $(SRC:.c=.o)
 
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: ntarik <ntarik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 20:00:43 by ntarik            #+#    #+#             */
-/*   Updated: 2024/05/25 19:20:13 by ntarik           ###   ########.fr       */
+/*   Updated: 2024/05/31 15:31:53 by ntarik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,21 @@
 #include <string.h>
 
 
-char	**ft_split(const char *str, char c);
-char **find_path(char **env);
-char	*ft_substr(const char *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *str);
-void	*ft_calloc(size_t count, size_t size);
-void	ft_bzero( void *dst, size_t n );
-void	*ft_memset(void *ptr, int x, size_t n);
-char	*ft_strjoin(char const *s1, char const *s2);
+int         pipe_it(int *fd);
+char        **is_cmd_composed(char *av);
+int         is_executable_1(int ac, char *av, char **envp);
+char        **find_path(char **env);
+int         check_my_in_files(char **av);
+int         check_my_out_files(int ac, char **av);
+int         redirecting_infile(int fd_in);
+int         redirecting_outfile(int fd_out);
+char        **ft_split(const char *str, char c);
+char        *ft_substr(const char *s, unsigned int start, size_t len);
+char        *ft_strdup(const char *s1);
+size_t      ft_strlen(const char *s);
+void        *ft_calloc(size_t count, size_t size);
+void        ft_bzero( void *dst, size_t n );
+void        *ft_memset(void *ptr, int x, size_t n);
+char        *ft_strjoin(char const *s1, char const *s2);
+
 #endif
